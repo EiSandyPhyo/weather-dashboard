9982afec77342526fdc261fe9a98adf52e90549f
EiSandyPhyo <eisandyphyo345@gmail.com>
2026-03-28 20:59:15 +0000

    Step1: File setup (React+Vite+Tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-sky-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-sky-700">Weather Dashboard</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
